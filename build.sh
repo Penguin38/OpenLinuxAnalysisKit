f8c74b99b0f5258993bdc3b433d3bc19e84e2033
Penguin <chenguanyou9338@gmail.com>
2024-03-26 23:47:02 +0800

    add build ARM64 config
    Signed-off-by: Penguin <chenguanyou9338@gmail.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 cmake -DCMAKE_C_COMPILER="/usr/bin/gcc" \
       -DCMAKE_CXX_COMPILER="/usr/bin/g++" \
       -DCMAKE_BUILD_TYPE="Debug" \
+      -DCMAKE_BUILD_TARGET_ARCH="arm64" \
       CMakeLists.txt \
       -B output
 
